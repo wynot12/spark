89e9e6b00cc1d8114cab6a0503ece1191750e43e
WooYeon <wylee.xyzi@gmail.com>
2019-09-16 07:27:06 +0900

    q01_simple

---
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/benchmark/Q01_simple.scala
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.execution.benchmark
+
+import org.apache.spark.sql.functions.{avg, count, sum, udf}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+/**
+ * TPC-H Query 11
+ * Savvas Savvides <savvas@purdue.edu>
+ *
+ */
+class Q01_simple {
+
+  def execute(sc: SparkSession): DataFrame = {
+
+    // this is used to implicitly convert an RDD to a DataFrame.
+    val sqlContext = sc.sqlContext
+    import sqlContext.implicits._
+
+    val decrease = udf { (x: Double, y: Double) => x * (1 - y) }
+    val increase = udf { (x: Double, y: Double) => x * (1 + y) }
+
+    val lineitem = TPCHQueryBenchmark.dfMap.get("joined").get
+
+    lineitem.filter($"l_shipdate" <= "1998-09-02")
+      .groupBy().sum()
+  }
+}
